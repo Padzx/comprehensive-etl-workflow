89df956ddd574a599f025dc07e0afd2b2ecad65f
Padzx <padzxgabriel@gmail.com>
2024-05-20 12:21:30 -0300

    Build Image Airflow - Github Actions

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -1,91 +1,61 @@
 #!/usr/bin/env bash
 
-# User-provided configuration must always be respected.
-#
-# Therefore, this script must only derive Airflow AIRFLOW__ variables from other variables
-# when the user did not provide their own configuration.
+# Define the wait_for_port function
+wait_for_port() {
+    local host="$1"
+    local port="$2"
+    local retry_seconds="${3:-5}"
+    local max_attempts="${4:-100}"
+    local attempt=1
 
-TRY_LOOP="20"
+    nc -z "$host" "$port"
+    status=$?
 
-# Global defaults and back-compat
+    until [[ "$status" -eq 0 ]]; do
+        if (( attempt == max_attempts )); then
+            echo "Failed to connect to $host:$port after $attempt attempts."
+            return 1
+        fi
+
+        echo "Attempt $attempt: $host:$port is not available yet"
+        attempt=$(( attempt + 1 ))
+        sleep "$retry_seconds"
+
+        nc -z "$host" "$port"
+        status=$?
+    done
+
+    echo "$host:$port is available"
+    return 0
+}
+
+# Set default values for environment variables
 : "${AIRFLOW_HOME:="/usr/local/airflow"}"
-: "${AIRFLOW__CORE__FERNET_KEY:=${FERNET_KEY:=$(python -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print(FERNET_KEY)")}}"
-: "${AIRFLOW__CORE__EXECUTOR:=${EXECUTOR:-Sequential}Executor}"
+: "${AIRFLOW__CORE__EXECUTOR:="${EXECUTOR:-SequentialExecutor}"}"
+: "${AIRFLOW__CORE__FERNET_KEY:="${FERNET_KEY:-$(python -c 'from cryptography.fernet import Fernet; print(Fernet.generate_key().decode())')}"}"
 
-# Load DAGs examples (default: Yes)
-if [[ -z "$AIRFLOW__CORE__LOAD_EXAMPLES" && "${LOAD_EX:=n}" == n ]]; then
-  AIRFLOW__CORE__LOAD_EXAMPLES=False
-fi
-
-export \
-  AIRFLOW_HOME \
-  AIRFLOW__CORE__EXECUTOR \
-  AIRFLOW__CORE__FERNET_KEY \
-  AIRFLOW__CORE__LOAD_EXAMPLES \
-
-# Install custom python package if requirements.txt is present
-if [ -e "/requirements.txt" ]; then
-    $(command -v pip) install --user -r /requirements.txt
-fi
-
-# Other executors than SequentialExecutor drive the need for an SQL database, here PostgreSQL is used
-if [ "$AIRFLOW__CORE__EXECUTOR" != "SequentialExecutor" ]; then
-  # Check if the user has provided explicit Airflow configuration concerning the database
-  if [ -z "$AIRFLOW__CORE__SQL_ALCHEMY_CONN" ]; then
-    # Default values corresponding to the default compose files
-    : "${POSTGRES_HOST:="postgres"}"
-    : "${POSTGRES_PORT:="5432"}"
-    : "${POSTGRES_USER:="airflow"}"
-    : "${POSTGRES_PASSWORD:="airflow"}"
-    : "${POSTGRES_DB:="airflow"}"
-    : "${POSTGRES_EXTRAS:-""}"
-
-    AIRFLOW__CORE__SQL_ALCHEMY_CONN="postgresql+psycopg2://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}${POSTGRES_EXTRAS}"
-    export AIRFLOW__CORE__SQL_ALCHEMY_CONN
-
-    # Check if the user has provided explicit Airflow configuration for the broker's connection to the database
-    if [ "$AIRFLOW__CORE__EXECUTOR" = "CeleryExecutor" ]; then
-      AIRFLOW__CELERY__RESULT_BACKEND="db+postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}${POSTGRES_EXTRAS}"
-      export AIRFLOW__CELERY__RESULT_BACKEND
-    fi
-  else
-    if [[ "$AIRFLOW__CORE__EXECUTOR" == "CeleryExecutor" && -z "$AIRFLOW__CELERY__RESULT_BACKEND" ]]; then
-      >&2 printf '%s\n' "FATAL: if you set AIRFLOW__CORE__SQL_ALCHEMY_CONN manually with CeleryExecutor you must also set AIRFLOW__CELERY__RESULT_BACKEND"
-      exit 1
-    fi
-
-    # Derive useful variables from the AIRFLOW__ variables provided explicitly by the user
-    POSTGRES_ENDPOINT=$(echo -n "$AIRFLOW__CORE__SQL_ALCHEMY_CONN" | cut -d '/' -f3 | sed -e 's,.*@,,')
-    POSTGRES_HOST=$(echo -n "$POSTGRES_ENDPOINT" | cut -d ':' -f1)
-    POSTGRES_PORT=$(echo -n "$POSTGRES_ENDPOINT" | cut -d ':' -f2)
-  fi
-
-  wait_for_port "Postgres" "$POSTGRES_HOST" "$POSTGRES_PORT"
-fi
+export AIRFLOW_HOME
+export AIRFLOW__CORE__EXECUTOR
+export AIRFLOW__CORE__FERNET_KEY
 
 case "$1" in
-  webserver)
-    airflow initdb
-    if [ "$AIRFLOW__CORE__EXECUTOR" = "LocalExecutor" ] || [ "$AIRFLOW__CORE__EXECUTOR" = "SequentialExecutor" ]; then
-      # With the "Local" and "Sequential" executors it should all run in one container.
-      airflow scheduler &
-    fi
-    exec airflow webserver
-    ;;
-  worker|scheduler)
-    # Give the webserver time to run initdb.
-    sleep 10
-    exec airflow "$@"
-    ;;
-  flower)
-    sleep 10
-    exec airflow "$@"
-    ;;
-  version)
-    exec airflow "$@"
-    ;;
-  *)
-    # The command is something like bash, not an airflow subcommand. Just run it in the right environment.
-    exec "$@"
-    ;;
+    webserver)
+        if [ "$AIRFLOW__CORE__EXECUTOR" = "CeleryExecutor" ]; then
+            wait_for_port "$REDIS_HOST" "$REDIS_PORT"
+            wait_for_port "$FLOWER_HOST" "$FLOWER_PORT"
+        fi
+        wait_for_port "$POSTGRES_HOST" "$POSTGRES_PORT"
+        airflow db upgrade
+        exec airflow webserver
+        ;;
+    worker|scheduler|flower)
+        wait_for_port "$POSTGRES_HOST" "$POSTGRES_PORT"
+        exec airflow "$@"
+        ;;
+    version)
+        exec airflow "$@"
+        ;;
+    *)
+        exec "$@"
+        ;;
 esac
