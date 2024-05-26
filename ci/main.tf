0a87318e0fd74007597488441d01101bffd9e620
Padzx <padzxgabriel@gmail.com>
2024-05-26 00:40:33 -0300

    Apache Airflow [UPDATED]

--- ci/main.tf
+++ ci/main.tf
@@ -1,4 +1,3 @@
-
 provider "aws" {
   region = "us-east-1"
 }
@@ -15,10 +14,12 @@ resource "aws_s3_bucket" "airflow_bucket" {
 
 resource "aws_s3_bucket_versioning" "airflow_bucket_versioning" {
   bucket = aws_s3_bucket.airflow_bucket.id
-  enabled = true
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
-resource "aws_s3_bucket_default_server_side_encryption_configuration" "airflow_bucket_encryption" {
+resource "aws_s3_bucket_server_side_encryption_configuration" "airflow_bucket_encryption" {
   bucket = aws_s3_bucket.airflow_bucket.id
 
   rule {
@@ -26,4 +27,13 @@ resource "aws_s3_bucket_default_server_side_encryption_configuration" "airflow_b
       sse_algorithm = "AES256"
     }
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "block_public_access" {
+  bucket = aws_s3_bucket.airflow_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
